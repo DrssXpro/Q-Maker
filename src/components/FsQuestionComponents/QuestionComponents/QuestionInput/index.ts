98492865939bee70277ad19d7fc01dbb07c09c40
Async <1827951482@qq.com>
2023-06-19 22:42:57 +0800

    docs: 更改Input注释错误

--- src/components/FsQuestionComponents/QuestionComponents/QuestionInput/index.ts
+++ src/components/FsQuestionComponents/QuestionComponents/QuestionInput/index.ts
@@ -3,13 +3,13 @@ import InputComponent from "./components/InputComponent";
 import InputPropsComponent from "./components/InputPropsComponent";
 import { IQuestionInputProps } from "./type";
 
-// Title 组件默认属性值（初始化、重置使用）
+// Input 组件默认属性值（初始化、重置使用）
 export const QuestionInputDefaultValue: IQuestionInputProps = {
   title: "输入框标题",
   placeholder: "请输入...",
 };
 
-// Title 组件基本配置
+// Input 组件基本配置
 export const QuestionInputConfig: IComponentConfig = {
   title: "输入框",
   type: "questionInput",
