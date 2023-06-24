c51044ecfce82c6575bbd3d0efd8bda9fa9a5d23
Async <1827951482@qq.com>
2023-06-24 20:55:29 +0800

    feat: 图标统计组件整合（后续整理）

--- src/components/FsQuestionComponents/QuestionComponents/QuestionCheckbox/components/CheckboxStatComponent.tsx
+++ src/components/FsQuestionComponents/QuestionComponents/QuestionCheckbox/components/CheckboxStatComponent.tsx
@@ -1,7 +1,22 @@
-import { FC } from "react";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { FC, useMemo } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
 import { IQuestionCheckboxStatProps } from "../type";
-
+function format(n: number) {
+  console.log(n);
+  return (n * 100).toFixed(2);
+}
+const STAT_COLORS = ["#FF2D2D", "#BE77FF", "#2894FF", "#00EC00", "#EAC100", "#FF9D6F"];
 const CheckboxStatComponent: FC<IQuestionCheckboxStatProps> = (props: IQuestionCheckboxStatProps) => {
   const { stat } = props;
   const test = [
@@ -18,28 +33,56 @@ const CheckboxStatComponent: FC<IQuestionCheckboxStatProps> = (props: IQuestionC
       count: 44,
     },
   ];
+
+  const sum = useMemo(() => {
+    let s = 0;
+    test.forEach((i: { name: string; count: number }) => (s += i.count));
+    return s;
+  }, [stat]);
   return (
-    <div style={{ width: "100%", height: "300px", marginTop: "20px" }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={400}
-          height={300}
-          data={test}
-          margin={{
-            top: 10,
-            right: 10,
-            left: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          {/* <Legend /> */}
-          <Bar dataKey="count" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+    <>
+      <div style={{ width: "100%", height: "200px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              dataKey={"count"}
+              data={test}
+              cx="50%" // x 轴的偏移
+              cy="50%" // y 轴的偏移
+              outerRadius={50} // 饼图的直径
+              fill="#8884d8"
+              label={(i) => `${i.name}: ${format(i.count / sum)}%`}
+            >
+              {test.map((_i, index) => {
+                return <Cell key={index} fill={STAT_COLORS[index]} />;
+              })}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div style={{ width: "100%", height: "300px", marginTop: "20px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={400}
+            height={300}
+            data={test}
+            margin={{
+              top: 10,
+              right: 10,
+              left: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            {/* <Legend /> */}
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </>
   );
 };
 
--- src/components/FsQuestionComponents/QuestionComponents/QuestionRadio/components/RadioStatComponent.tsx
+++ src/components/FsQuestionComponents/QuestionComponents/QuestionRadio/components/RadioStatComponent.tsx
@@ -1,5 +1,16 @@
 import { FC, useMemo } from "react";
-import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+} from "recharts";
 import { IQuestionRadioStatProps } from "../type";
 
 const STAT_COLORS = ["#FF2D2D", "#BE77FF", "#2894FF", "#00EC00", "#EAC100", "#FF9D6F"];
@@ -32,26 +43,49 @@ const RadioStatComponent: FC<IQuestionRadioStatProps> = (props: IQuestionRadioSt
   }, [stat]);
 
   return (
-    <div style={{ width: "100%", height: "200px" }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart>
-          <Pie
-            dataKey={"count"}
+    <>
+      <div style={{ width: "100%", height: "200px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              dataKey={"count"}
+              data={test}
+              cx="50%" // x 轴的偏移
+              cy="50%" // y 轴的偏移
+              outerRadius={50} // 饼图的直径
+              fill="#8884d8"
+              label={(i) => `${i.name}: ${format(i.count / sum)}%`}
+            >
+              {test.map((_i, index) => {
+                return <Cell key={index} fill={STAT_COLORS[index]} />;
+              })}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div style={{ width: "100%", height: "300px", marginTop: "20px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={400}
+            height={300}
             data={test}
-            cx="50%" // x 轴的偏移
-            cy="50%" // y 轴的偏移
-            outerRadius={50} // 饼图的直径
-            fill="#8884d8"
-            label={(i) => `${i.name}: ${format(i.count / sum)}%`}
+            margin={{
+              top: 10,
+              right: 10,
+              left: 0,
+            }}
           >
-            {test.map((_i, index) => {
-              return <Cell key={index} fill={STAT_COLORS[index]} />;
-            })}
-          </Pie>
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
-    </div>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            {/* <Legend /> */}
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </>
   );
 };
 
